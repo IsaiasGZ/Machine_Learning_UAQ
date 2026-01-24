94be193b1d28ed04bcf90681eab1d91ff5c2686c
Isaias Garcia Zanella <isaiasgz2018@outlook.com>
2026-01-23 18:23:24 -0600

    version 0.1

---
+++ Programming/Tarea1.py
@@ -0,0 +1,8 @@
+# En esta actividad debemos generar una herramienta que al momento de insertar alguna B.D. #
+# que sea de tipo estructurada realizar los siguientes calculos : #
+#
+#
+#
+#
+#
+print('Hola mundo!!')
